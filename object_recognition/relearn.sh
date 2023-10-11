5578db2c7ccdcbc5315cee8d2856fb7f778fb676
ultragreed <agent002004@gmail.com>
2023-10-12 00:18:11 +1000

    removed extra files from object_recognition

--- object_recognition/relearn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-(
-  export PYTHONPATH=$(realpath ..)
-
-  python learn.py && 
-  python normalize.py && 
-  python histograms.py && 
-  python checker.py
-)
--- object_recognition/renormalize.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-(
-  export PYTHONPATH=$(realpath ..)
-
-  python normalize.py && 
-  python histograms.py && 
-  python checker.py
-)
